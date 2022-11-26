99febd4574e2b00bf21ebfa7f9e94872831e1085
ErikHenrysson <henryssonerik@gmail.com>
2022-11-26 17:52:00 +0900

    Added check accuracy for stl and created the STLnb

---
+++ my_func.py
@@ -0,0 +1,94 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+def my_train(model, num_epochs, train_loader, criterion, optimizer, device):
+        for epoch in range(num_epochs):
+            for batch_idx, (data, targets) in enumerate(train_loader):
+                data = data.to(device = device)
+                targets = targets.to(device = device)
+                scores = model(data)
+                loss = criterion(scores, targets)
+
+                optimizer.zero_grad()
+                loss.backward()
+
+                optimizer.step()
+            
+def check_accuracy(loader, model, device):
+    if loader.dataset.train:
+        print("Checking accuracy on the training data")
+    else:
+        print("Checking accuracy on test data")
+
+    num_correct = 0
+    num_samples = 0
+    model.eval()
+
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device = device)
+            y = y.to(device = device)
+
+            scores = model(x)
+            _, predictions = scores.max(1)
+            num_correct += (predictions == y).sum()
+            num_samples += predictions.size(0)
+
+        print(f'Got {num_correct} / {num_samples} with accuracy {float(num_correct)/float(num_samples)*100:.2f}')
+
+    model.train()
+
+def check_STLaccuracy(loader, model, device):
+    if loader.dataset.split == 'train':
+        print("Checking accuracy on the training data")
+    else:
+        print("Checking accuracy on test data")
+
+    num_correct = 0
+    num_samples = 0
+    model.eval()
+
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device = device)
+            y = y.to(device = device)
+
+            scores = model(x)
+            _, predictions = scores.max(1)
+            num_correct += (predictions == y).sum()
+            num_samples += predictions.size(0)
+
+        print(f'Got {num_correct} / {num_samples} with accuracy {float(num_correct)/float(num_samples)*100:.2f}')
+
+    model.train()
+
+
+
+def check_accuracy_by_class(classes, test_loader, device, model):
+    # prepare to count predictions for each class
+    correct_pred = {classname: 0 for classname in classes}
+    total_pred = {classname: 0 for classname in classes}
+
+    # again no gradients needed
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data[0].to(device), data[1].to(device)
+            outputs = model(images)
+            _, predictions = torch.max(outputs, 1)
+            
+            # collect the correct predictions for each class
+            for label, prediction in zip(labels, predictions):
+                if label == prediction:
+                    correct_pred[classes[label]] += 1
+                total_pred[classes[label]] += 1
+
+
+    # print accuracy for each class
+    for classname, correct_count in correct_pred.items():
+        accuracy = 100 * float(correct_count) / total_pred[classname]
+        print(f'Accuracy for class: {classname:5s} is {accuracy:.1f} %')
